3175874ac6a1e220ca964834d579d18389f490bb
Teodorescu Mihail <mihail.teodorescu>
2019-05-22 16:35:28 +0300

    commit stage A

--- DellChallenge/DellChallenge.A/Program.cs
+++ DellChallenge/DellChallenge.A/Program.cs
@@ -7,6 +7,9 @@ namespace DellChallenge.A
         static void Main(string[] args)
         {
             // State and explain console output order.
+
+            // because B is derived from A, will print `A.A()` and then `B.B()`
+            // and then setting Age = 0 will result another print like `A .Age`
             new B();
             Console.ReadKey();
         }
