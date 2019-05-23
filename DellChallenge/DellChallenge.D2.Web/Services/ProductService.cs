346b4fdf3ddbfe1314ff8ba5d4ab70feaedefdd9
Teodorescu Mihail <mihail.teodorescu>
2019-05-23 15:06:16 +0300

    improve ProductService

--- DellChallenge/DellChallenge.D2.Web/Services/ProductService.cs
+++ DellChallenge/DellChallenge.D2.Web/Services/ProductService.cs
@@ -1,52 +1,70 @@
-﻿using System.Collections.Generic;
-using DellChallenge.D2.Web.Models;
+﻿using DellChallenge.D2.Web.Models;
 using RestSharp;
+using System.Collections.Generic;
 
 namespace DellChallenge.D2.Web.Services
 {
     public class ProductService : IProductService
     {
         private const string REST_API = "http://localhost:2534/api";
+        private const string RESOURCE_NAME = "products";
+        private readonly RestClient _apiClient;
+
+        public ProductService()
+        {
+            _apiClient = new RestClient(REST_API);
+        }
 
         public ProductModel Add(NewProductModel newProduct)
         {
-            var apiClient = new RestClient(REST_API);
-            var apiRequest = new RestRequest("products", Method.POST, DataFormat.Json);
-            apiRequest.AddJsonBody(newProduct);
-            var apiResponse = apiClient.Execute<ProductModel>(apiRequest);
-            return apiResponse.Data;
+            return Execute<ProductModel, NewProductModel>(RESOURCE_NAME, Method.POST, newProduct);
         }
 
         public IEnumerable<ProductModel> GetAll()
         {
-            var apiClient = new RestClient(REST_API);
-            var apiRequest = new RestRequest("products", Method.GET, DataFormat.Json);
-            var apiResponse = apiClient.Execute<List<ProductModel>>(apiRequest);
-            return apiResponse.Data;
+            return Execute<List<ProductModel>>(RESOURCE_NAME, Method.GET);
         }
 
         public ProductModel GetById(string id)
         {
-            var apiClient = new RestClient(REST_API);
-            var apiRequest = new RestRequest($"products/{id}", Method.GET, DataFormat.Json);
-            var apiResponse = apiClient.Execute<ProductModel>(apiRequest);
-            return apiResponse.Data;
+            return Execute<ProductModel>($"{RESOURCE_NAME}/{id}", Method.GET);
         }
 
         public ProductModel Update(string id, NewProductModel newProduct)
         {
-            var apiClient = new RestClient(REST_API);
-            var apiRequest = new RestRequest($"products/{id}", Method.PUT, DataFormat.Json);
-            apiRequest.AddJsonBody(newProduct);
-            var apiResponse = apiClient.Execute<ProductModel>(apiRequest);
-            return apiResponse.Data;
+            return Execute<ProductModel, NewProductModel>($"{RESOURCE_NAME}/{id}", Method.PUT, newProduct);
         }
 
         public void Delete(string id)
         {
-            var apiClient = new RestClient(REST_API);
-            var apiRequest = new RestRequest($"products/{id}", Method.DELETE, DataFormat.Json);
-            var apiResponse = apiClient.Execute<ProductModel>(apiRequest);
+            Execute($"{RESOURCE_NAME}/{id}", Method.DELETE);
+        }
+
+        private T Execute<T>(string url, Method method, T model) where T : new()
+        {
+            return Execute<T, T>(url, method, model);
+        }
+
+        private T Execute<T>(string url, Method method) where T : new()
+        {
+            return Execute<T>(url, method, default(T));
+        }
+
+        private T1 Execute<T1, T2>(string url, Method method, T2 model) where T1 : new() where T2 : new()
+        {
+            var apiRequest = new RestRequest(url, method, DataFormat.Json);
+            if (model != null)
+            {
+                apiRequest.AddJsonBody(model);
+            }
+
+            var apiResponse = _apiClient.Execute<T1>(apiRequest);
+            return apiResponse.Data;
+        }
+
+        private void Execute(string url, Method method)
+        {
+            Execute<bool?>(url, method, null);
         }
     }
 }
